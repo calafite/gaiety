5cacbfad1dd63dbddc3ff78decc263a87a581ce4
PertinaciousMedici <pertinaciouslymedici@gmail.com>
2026-05-30 23:45:58 +0000

    feat: define manifest structure

---
+++ src/manifest.rs
@@ -0,0 +1,32 @@
+use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Debug, Deserialize)]
+pub struct Manifest {
+    pub module: ModuleMeta,
+    #[serde(default)]
+    pub api: ApiMeta,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct ModuleMeta {
+    pub name: String,
+    pub description: Option<String>,
+    pub version: String,
+    #[serde(default)]
+    pub deps: Vec<String>,
+    #[serde(default)]
+    pub tags: Vec<String>,
+    #[serde(default)]
+    pub requires_cmd: Vec<String>,
+}
+
+#[derive(Debug, Deserialize, Default)]
+pub struct ApiMeta {
+    #[serde(default)]
+    pub functions: Vec<String>,
+    #[serde(default)]
+    pub variables: Vec<String>,
+    #[serde(default)]
+    pub aliases: HashMap<String, String>,
+}
